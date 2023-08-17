29b0eafcd2a952b8635c626ca1966c490f4cb144
Tom Lightfoot <tomlightfootr@gmail.com>
2023-08-18 11:59:52 +1200

    Primitive system to display games
    Game list page now displays games from the csv file, although very primitive.

--- games/blueprints/game_library_bp.py
+++ games/blueprints/game_library_bp.py
@@ -1,7 +1,19 @@
 from flask import Blueprint, render_template
+from ..adapters.datareader.csvdatareader import GameFileCSVReader
 
 game_library_bp = Blueprint('game_library_bp', __name__)
 
 @game_library_bp.route('/game_library')
 def game_library():
-    return render_template('game_library.html')
+
+    MAX_GAMES_PER_PAGE = 10
+
+    #create instance of GameFileCSVReader
+    file_reader = GameFileCSVReader("games/adapters/data/games.csv")
+    #read csv file
+    file_reader.read_csv_file()
+
+    #store list of all game objects
+    games_dataset = file_reader.dataset_of_games
+
+    return render_template('game_library.html', games_dataset = games_dataset[:MAX_GAMES_PER_PAGE - 1])
