bc62086f0609cb1dbf79d1094edaee6ba0783cc0
Tom Lightfoot <tomlightfootr@gmail.com>
2023-08-18 16:44:48 +1200

    added page turning functionality
    can cycle through pages in game library

--- games/blueprints/game_library_bp.py
+++ games/blueprints/game_library_bp.py
@@ -3,10 +3,11 @@ from ..adapters.datareader.csvdatareader import GameFileCSVReader
 
 game_library_bp = Blueprint('game_library_bp', __name__)
 
-@game_library_bp.route('/game_library')
-def game_library():
+@game_library_bp.route('/game_library/<current_page>')
+def game_library(current_page):
 
-    MAX_GAMES_PER_PAGE = 10
+    current_page = int(current_page)
+    items_per_page = 10
 
     #create instance of GameFileCSVReader
     file_reader = GameFileCSVReader("games/adapters/data/games.csv")
@@ -16,4 +17,8 @@ def game_library():
     #store list of all game objects
     games_dataset = file_reader.dataset_of_games
 
-    return render_template('game_library.html', games_dataset = games_dataset[:MAX_GAMES_PER_PAGE - 1])
+    return render_template(
+        'game_library.html',
+        current_page=current_page,
+        games_dataset=games_dataset[current_page * items_per_page:current_page * items_per_page + items_per_page]
+        )
