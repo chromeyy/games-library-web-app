601ab789fffef3d07855be7b3ce0fd9f2b2beee2
Tom Lightfoot <tomlightfootr@gmail.com>
2023-08-20 15:14:16 +1200

    Genre selection (url only)
    A genre can now be typed into the url to filter the results

--- games/blueprints/game_library_bp.py
+++ games/blueprints/game_library_bp.py
@@ -3,8 +3,8 @@ from ..adapters.datareader.csvdatareader import GameFileCSVReader
 
 game_library_bp = Blueprint('game_library_bp', __name__)
 
-@game_library_bp.route('/game_library/<current_page>')
-def game_library(current_page):
+@game_library_bp.route('/game_library/<selected_genre>/<current_page>')
+def game_library(selected_genre, current_page):
 
     current_page = int(current_page)
     items_per_page = 10
@@ -14,11 +14,20 @@ def game_library(current_page):
     #read csv file
     file_reader.read_csv_file()
 
-    #store list of all game objects
-    games_dataset = file_reader.dataset_of_games
+    if selected_genre == '_':
+        games_dataset = file_reader.dataset_of_games
+    else:
+        games_dataset = []
+        for game in file_reader.dataset_of_games:
+            for genre in game.genres:
+                if genre.genre_name == selected_genre:
+                    games_dataset.append(game)
+                    break
 
     return render_template(
         'game_library.html',
+        selected_genre=selected_genre,
         current_page=current_page,
         games_dataset=games_dataset[current_page * items_per_page:current_page * items_per_page + items_per_page]
         )
+
