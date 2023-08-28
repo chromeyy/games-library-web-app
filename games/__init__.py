e93b5d1c078284012ce603d094cf5430b19c3715
Kevin Jiang <kevinj1478@gmail.com>
2023-08-29 11:42:50 +1200

    changes to directory and init

--- games/__init__.py
+++ games/__init__.py
@@ -4,10 +4,12 @@ from pathlib import Path
 import games.adapters.repository as repo
 from games.adapters.memory_repository import MemoryRepository, populate
 
-from flask import Flask, render_template, Blueprint
-from .blueprints.index_bp import index_bp
-from .blueprints.game_library_bp import game_library_bp
-from .blueprints.game_info_bp import game_info_bp
+from flask import Flask
+
+
+# from .blueprints.index_bp import index_bp
+
+
 
 
 
@@ -17,13 +19,31 @@ def create_app():
     # Create the Flask app object.
     app = Flask(__name__)
 
+    # # Configure the app from configuration-file settings.
+    # app.config.from_object('config.Config')
+
     data_path = Path('games') / 'adapters' / 'data'
 
+    # if test_config is not None:
+    #     # Load test configuration, and override any configuration settings.
+    #     app.config.from_mapping(test_config)
+    #     data_path = app.config['TEST_DATA_PATH']
+
+    # Create the MemoryRepository implementation for a memory-based repository.
     repo.repo_instance = MemoryRepository()
+    # fill the content of the repository from the provided csv files
     populate(data_path, repo.repo_instance)
 
-    app.register_blueprint(index_bp)
-    app.register_blueprint(game_library_bp)
-    app.register_blueprint(game_info_bp)
+    # Build the application - these steps require an application context.
+    with app.app_context():
+        # Register blueprints.
+        from games.index.index_bp import index_bp
+        app.register_blueprint(index_bp)
+
+        from games.game_library.game_library_bp import game_library_bp
+        app.register_blueprint(game_library_bp)
+
+        from games.game_info.game_info_bp import game_info_bp
+        app.register_blueprint(game_info_bp)
 
     return app
