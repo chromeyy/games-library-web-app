6766cf7c6f5d6ad323e4d030b8e63095469d2455
Kevin Jiang <kevinj1478@gmail.com>
2023-08-29 12:35:22 +1200

    Update memory_repository.py
    Implemented abstractRepository's new methods get games by genre publisher and search term

--- games/adapters/memory_repository.py
+++ games/adapters/memory_repository.py
@@ -3,11 +3,12 @@ from pathlib import Path
 from games.domainmodel.model import Game
 from games.adapters.datareader.csvdatareader import GameFileCSVReader
 
+
 class MemoryRepository(abstract_repo.AbstractRepository):
     def __init__(self):
         self.__games = list()
 
-    def get_games(self, selected_genre='all', selected_publisher='all'):
+    def get_games_by_genre(self, selected_genre='all'):
         if selected_genre == 'all':
             games_dataset = self.__games.copy()
         else:
@@ -17,21 +18,50 @@ class MemoryRepository(abstract_repo.AbstractRepository):
                     if genre.genre_name == selected_genre:
                         games_dataset.append(game)
                         break
+        return games_dataset
 
+    def get_games_by_publisher(self, selected_publisher='all'):
         if selected_publisher == 'all':
-            pass
+            games_dataset = self.__games.copy()
         else:
-            new_dataset = []
-            for game in games_dataset:
+            games_dataset = list()
+            for game in self.__games:
                 if game.publisher.publisher_name == selected_publisher:
-                    new_dataset.append(game)
-            games_dataset = new_dataset
+                    games_dataset.append(game)
+        return games_dataset
 
+    def get_games_by_search(self, search_term=""):
+        if search_term == "":
+            games_dataset = self.__games.copy()
+        else:
+            games_dataset = list()
+            for game in self.__games:
+                game_added = False
+
+                if (search_term in game.title) and not game_added:
+                    games_dataset.append(game)
+                    game_added = True
+
+                if not game_added:
+                    for genre in game.genres:
+                        if search_term in genre.genre_name:
+                            games_dataset.append(game)
+                            game_added = True
+                            break
+
+                if search_term in game.publisher and not game_added:
+                    games_dataset.append(game)
+                    game_added = True
+
+                if search_term in game.description and not game_added:
+                    games_dataset.append(game)
+                    # game_added = True
         return games_dataset
 
     def add_games(self, game):
         if isinstance(game, Game):
             self.__games.append(game)
+
 
 def populate(data_path, repo: MemoryRepository):
     filename = str(Path(data_path) / "games.csv")
