8a3158d550d37142930725e1b4af10e68172e344
Tom Lightfoot <tomlightfootr@gmail.com>
2023-08-29 13:29:27 +1200

    route changes

--- games/game_library/game_library_bp.py
+++ games/game_library/game_library_bp.py
@@ -36,26 +36,6 @@ def game_library_genre():
     return render_template(
         'game_library.html',
         selected_genre=selected_genre,
-        selected_publisher='all',
-        current_page=current_page,
-        games_dataset=games_dataset[current_page * items_per_page:current_page * items_per_page + items_per_page]
-        )
-
-@game_library_bp.route('/game_library/publisher')
-def game_library_publisher():
-
-    selected_publisher = request.args.get('selected_publisher')
-    current_page = request.args.get('current_page')
-
-    current_page = int(current_page)
-    items_per_page = 10
-
-    games_dataset = repo.repo_instance.get_games_by_publisher(selected_publisher)
-
-    return render_template(
-        'game_library.html',
-        selected_genre='all',
-        selected_publisher=selected_publisher,
         current_page=current_page,
         games_dataset=games_dataset[current_page * items_per_page:current_page * items_per_page + items_per_page]
         )
