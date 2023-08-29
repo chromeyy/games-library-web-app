560c3c80b240bb0e03f75389975c6cf3d45534e0
Kevin Jiang <kevinj1478@gmail.com>
2023-08-29 14:29:35 +1200

    created search form created search in game library blueprint

--- games/game_library/game_library_bp.py
+++ games/game_library/game_library_bp.py
@@ -4,6 +4,7 @@ import games.adapters.repository as repo
 
 game_library_bp = Blueprint('game_library_bp', __name__)
 
+
 @game_library_bp.route('/game_library')
 def game_library():
 
@@ -17,10 +18,30 @@ def game_library():
     return render_template(
         'game_library.html',
         selected_genre='all',
-        selected_publisher='all',
         current_page=current_page,
         games_dataset=games_dataset[current_page * items_per_page:current_page * items_per_page + items_per_page]
         )
+
+
+@game_library_bp.route('/game_library/search', methods=['GET', 'POST'])
+def game_library_search_term():
+    if request.method == 'POST':
+        result = request.form
+
+        current_page = request.args.get('current_page')
+
+        current_page = int(current_page)
+        items_per_page = 10
+        games_dataset = repo.repo_instance.get_games_by_search(result["search_category"], result["search_term"])
+
+        return render_template(
+            'game_library.html',
+            selected_genre='all',
+            current_page=current_page,
+            games_dataset=games_dataset[current_page * items_per_page:current_page * items_per_page + items_per_page],
+            result=result
+            )
+
 
 @game_library_bp.route('/game_library/genre')
 def game_library_genre():
