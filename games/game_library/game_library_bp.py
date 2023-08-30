d6ab22d8ddb1431b73b5e5a82171bb91d338dbdf
Tom Lightfoot <tomlightfootr@gmail.com>
2023-08-30 14:08:20 +1200

    buttons only visible if page available

--- games/game_library/game_library_bp.py
+++ games/game_library/game_library_bp.py
@@ -12,14 +12,14 @@ def game_library():
 
     current_page = int(current_page)
     items_per_page = 10
-
-    games_dataset = services.get_games_by_genre('all')
-    services.alpha_sort_games(games_dataset)
+    last_page_number = services.get_last_page_num(services.get_num_of_games_in_genre("all"), items_per_page)
 
     if current_page < 0:
         current_page = 0
-    if current_page * items_per_page > len(games_dataset):
-        current_page = len(games_dataset) // items_per_page
+    if current_page * items_per_page > services.get_num_of_games_in_genre('all'):
+        current_page = last_page_number
+
+    games_dataset = services.get_games_by_genre('all', current_page, items_per_page)
 
     genres = services.get_genres()
     services.alpha_sort_genres(genres)
@@ -28,8 +28,9 @@ def game_library():
         'game_library.html',
         selected_genre='all',
         current_page=current_page,
-        games_dataset=games_dataset[current_page * items_per_page:current_page * items_per_page + items_per_page],
-        genres=genres
+        games_dataset=games_dataset,
+        genres=genres,
+        last_page_number=last_page_number
         )
 
 
@@ -46,13 +47,15 @@ def game_library_search_term():
 
     current_page = int(current_page)
     items_per_page = 10
-    games_dataset = services.get_games_by_search(search_category, search_term)
-    services.alpha_sort_games(games_dataset)
+    last_page_number = services.get_last_page_num(services.get_num_of_games_in_search(search_category, search_term),
+                                                  items_per_page)
 
     if current_page < 0:
         current_page = 0
-    if current_page * items_per_page > len(games_dataset):
-        current_page = len(games_dataset) // items_per_page
+    if current_page * items_per_page > services.get_num_of_games_in_search(search_category, search_term):
+        current_page = last_page_number
+
+    games_dataset = services.get_games_by_search(search_category, search_term, current_page, items_per_page)
 
     genres = services.get_genres()
     services.alpha_sort_genres(genres)
@@ -61,10 +64,11 @@ def game_library_search_term():
         'game_library.html',
         selected_genre='all',
         current_page=current_page,
-        games_dataset=games_dataset[current_page * items_per_page:current_page * items_per_page + items_per_page],
+        games_dataset=games_dataset,
         search_category=search_category,
         search_term=search_term,
-        genres=genres
+        genres=genres,
+        last_page_number=last_page_number
         )
 
 
@@ -77,14 +81,14 @@ def game_library_genre():
 
     current_page = int(current_page)
     items_per_page = 10
-
-    games_dataset = services.get_games_by_genre(selected_genre)
-    services.alpha_sort_games(games_dataset)
+    last_page_number = services.get_last_page_num(services.get_num_of_games_in_genre(selected_genre), items_per_page)
 
     if current_page < 0:
         current_page = 0
-    if current_page * items_per_page > len(games_dataset):
-        current_page = len(games_dataset) // items_per_page
+    if current_page * items_per_page > services.get_num_of_games_in_genre(selected_genre):
+        current_page = last_page_number
+
+    games_dataset = services.get_games_by_genre(selected_genre, current_page, items_per_page)
 
     genres = services.get_genres()
     services.alpha_sort_genres(genres)
@@ -93,7 +97,8 @@ def game_library_genre():
         'game_library.html',
         selected_genre=selected_genre,
         current_page=current_page,
-        games_dataset=games_dataset[current_page * items_per_page:current_page * items_per_page + items_per_page],
-        genres=genres
+        games_dataset=games_dataset,
+        genres=genres,
+        last_page_number=last_page_number
         )
 
--- games/game_library/services.py
+++ games/game_library/services.py
@@ -1,13 +1,29 @@
 import games.adapters.repository as repo
 
-def get_games_by_genre(genre):
-    return repo.repo_instance.get_games_by_genre(genre)
+def get_games_by_genre(genre, current_page, items_per_page):
+    games = repo.repo_instance.get_games_by_genre(genre)
+    alpha_sort_games(games)
+    return games[current_page * items_per_page:current_page * items_per_page + items_per_page]
 
-def get_games_by_search(search_category, search_term):
-    return repo.repo_instance.get_games_by_search(search_category, search_term)
+def get_games_by_search(search_category, search_term, current_page, items_per_page):
+    games = repo.repo_instance.get_games_by_search(search_category, search_term)
+    alpha_sort_games(games)
+    return games[current_page * items_per_page:current_page * items_per_page + items_per_page]
 
 def get_genres():
     return repo.repo_instance.get_list_of_genres()
+
+def get_num_of_games_in_genre(genre):
+    return len(repo.repo_instance.get_games_by_genre(genre))
+
+def get_num_of_games_in_search(search_category, search_term):
+    return len(repo.repo_instance.get_games_by_search(search_category, search_term))
+
+def get_last_page_num(num_of_games, items_per_page):
+    return num_of_games // items_per_page
+
+def get_num_of_genres():
+    return len(repo.repo_instance.get_list_of_genres())
 
 def alpha_sort_games(games):
     games.sort(key=lambda game: game.title)
