8cd0b944d953aa31d52182d738c6842affb9dde6
Kevin Jiang <kevinj1478@gmail.com>
2023-08-30 17:23:52 +1200

    changes to the services parameters to contain the repo and adjusted all uses in the blueprints to give the repo instance as a paramater + added testing for services

--- games/game_library/services.py
+++ games/game_library/services.py
@@ -1,32 +1,41 @@
-import games.adapters.repository as repo
+from games.adapters.repository import AbstractRepository
 
-def get_games_by_genre(genre, current_page, items_per_page):
-    games = repo.repo_instance.get_games_by_genre(genre)
+
+def get_games_by_genre(genre, current_page, items_per_page, repo: AbstractRepository):
+    games = repo.get_games_by_genre(genre)
     alpha_sort_games(games)
     return games[current_page * items_per_page:current_page * items_per_page + items_per_page]
 
-def get_games_by_search(search_category, search_term, current_page, items_per_page):
-    games = repo.repo_instance.get_games_by_search(search_category, search_term)
+
+def get_games_by_search(search_category, search_term, current_page, items_per_page, repo: AbstractRepository):
+    games = repo.get_games_by_search(search_category, search_term)
     alpha_sort_games(games)
     return games[current_page * items_per_page:current_page * items_per_page + items_per_page]
 
-def get_genres():
-    return repo.repo_instance.get_list_of_genres()
 
-def get_num_of_games_in_genre(genre):
-    return len(repo.repo_instance.get_games_by_genre(genre))
+def get_genres(repo: AbstractRepository):
+    return repo.get_list_of_genres()
 
-def get_num_of_games_in_search(search_category, search_term):
-    return len(repo.repo_instance.get_games_by_search(search_category, search_term))
+
+def get_num_of_games_in_genre(genre, repo: AbstractRepository):
+    return len(repo.get_games_by_genre(genre))
+
+
+def get_num_of_games_in_search(search_category, search_term, repo: AbstractRepository):
+    return len(repo.get_games_by_search(search_category, search_term))
+
 
 def get_last_page_num(num_of_games, items_per_page):
     return num_of_games // items_per_page
 
-def get_num_of_genres():
-    return len(repo.repo_instance.get_list_of_genres())
+
+def get_num_of_genres(repo: AbstractRepository):
+    return len(repo.get_list_of_genres())
+
 
 def alpha_sort_games(games):
     games.sort(key=lambda game: game.title)
+
 
 def alpha_sort_genres(genres):
     genres.sort(key=lambda genre: genre.genre_name)
--- tests/unit/test_service_layer.py
+++ tests/unit/test_service_layer.py
@@ -18,12 +18,21 @@ def list_of_genres():
 
 # testing services for game_info
 def test_game_info_retrieve_game(in_memory_repo):
-    pass
+    game = games_info_services.get_game_by_id(267360, in_memory_repo)
+    assert game == Game(267360, "MURI")
 
 
 # testing services for game_library
 def test_game_library_retrieve_game_by_genre(in_memory_repo):
-    pass
+    games = games_library_services.get_games_by_genre("RPG", 1, 10, in_memory_repo)
+    assert (str(sorted(games)[:3]) ==
+            "[<Game 648510, Choice of the Star Captain>, <Game 656740, Cosmonator>, <Game 658970, CasinoRPG>]")
+    assert len(games) == 10
+    assert len(games_library_services.get_games_by_genre(
+        "Invalid", 1, 10, in_memory_repo)) == 0
+    games_second_page = games_library_services.get_games_by_genre("RPG", 3, 3, in_memory_repo)
+    assert (str(games_second_page) ==
+            "[<Game 2011540, Blue Star Mobile Team>, <Game 926140, CHANGE: A Homeless Survival Experience>, <Game 658970, CasinoRPG>]")
 
 
 def test_game_library_retrieve_game_by_search(in_memory_repo):
@@ -43,7 +52,9 @@ def test_game_library_get_num_of_games_in_genre(in_memory_repo):
 
 
 def test_game_library_get_last_page_num(in_memory_repo):
-    pass
+    assert games_library_services.get_last_page_num(3, 2) == 2
+    assert games_library_services.get_last_page_num(3, 3) == 1
+    assert games_library_services.get_last_page_num(4, 2) == 2
 
 
 def test_game_library_get_num_of_genres(in_memory_repo):
