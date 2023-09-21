7473c5e6f03a366c2362f70daaa84b719f988835
Kevin Jiang <kevinj1478@gmail.com>
2023-09-21 22:04:39 +1200

    Created review functionality but does not show all comments

--- games/game_info/game_info_bp.py
+++ games/game_info/game_info_bp.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, request
 import games.game_info.services as services
 import games.adapters.repository as repo
 
+from games.authentication.authentication_bp import login_required
+
 game_info_bp = Blueprint('game_info_bp', __name__)
 
 
@@ -11,5 +13,12 @@ def game_info():
     game_id = request.args.get('game_id')
     game = services.get_game_by_id(game_id, repo.repo_instance)
 
-    return render_template('game_info.html', game=game)
+    return render_template('game_view/game_info.html', game=game)
 
+@game_info_bp.route('/game_info/review_game')
+@login_required
+def review_game():
+    game_id = request.args.get('game_id')
+    game = services.get_game_by_id(game_id, repo.repo_instance)
+
+    return render_template('game_view/review.html', game=game)
