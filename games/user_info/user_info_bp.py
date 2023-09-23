61e8541e76b1c400f2d1d75eb18f0458db3d155a
Kevin Jiang <kevinj1478@gmail.com>
2023-09-23 20:58:50 +1200

    Fixed some bugs for user_info

--- games/user_info/user_info_bp.py
+++ games/user_info/user_info_bp.py
@@ -1,13 +1,18 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session
 import games.user_info.services as services
 import games.adapters.repository as repo
+
+from games.authentication.authentication_bp import login_required
 
 user_info_bp = Blueprint('user_info_bp', __name__)
 
 
 @user_info_bp.route('/user_info')
+@login_required
 def user_info():
     username = request.args.get('username')
+    if username is None and 'user_name' in session:
+        username = session['user_name']
     user = services.get_user(username, repo.repo_instance)
 
     return render_template('user_info.html', user=user)
