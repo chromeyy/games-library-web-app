e47db4d6e28d28a6a26f6d0e4543b4a2891fa878
tlightfootr <tomlightfootr@gmail.com>
2023-10-09 21:30:20 +1300

    init file database code (NOT WORKING)

--- games/__init__.py
+++ games/__init__.py
@@ -4,8 +4,15 @@ from pathlib import Path
 import games.adapters.repository as repo
 from games.adapters.memory_repository import MemoryRepository
 from games.adapters.repository_populate import populate
+from games.adapters import database_repository, repository_populate
 
 from flask import Flask
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, clear_mappers
+from sqlalchemy.pool import NullPool
+
+from games.adapters.orm import metadata, map_model_to_tables
 
 
 def create_app(test_config=None):
@@ -24,10 +31,45 @@ def create_app(test_config=None):
         app.config.from_mapping(test_config)
         data_path = app.config['TEST_DATA_PATH']
 
-    # Create the MemoryRepository implementation for a memory-based repository.
-    repo.repo_instance = MemoryRepository()
-    # fill in the content of the repository from the provided csv files
-    populate(data_path, repo.repo_instance)
+    if  app.config['REPOSITORY'] == 'memory':
+        # Create the MemoryRepository implementation for a memory-based repository.
+        repo.repo_instance = MemoryRepository()
+        # fill in the content of the repository from the provided csv files
+        populate(data_path, repo.repo_instance)
+
+    elif app.config['REPOSITORY'] == 'database':
+        # Configure database.
+        database_uri = app.config['SQLALCHEMY_DATABASE_URI']
+
+        # We create a SQLite database, which is based on a single file.
+        # leading to a URI of "sqlite:///games.db".
+        database_echo = app.config['SQLALCHEMY_ECHO']
+        # Please do not change the settings for connect_args and poolclass!
+        database_engine = create_engine(database_uri, connect_args={"check_same_thread": False}, poolclass=NullPool,
+                                        echo=database_echo)
+
+        # Create the database session factory using sessionmaker (this has to be done once, in a global manner)
+        session_factory = sessionmaker(autocommit=False, autoflush=True, bind=database_engine)
+        # Create the SQLAlchemy DatabaseRepository instance for an sqlite3-based repository.
+        repo.repo_instance = database_repository.SqlAlchemyRepository(session_factory)
+
+        if app.config['TESTING'] == 'True' or len(database_engine.table_names()) == 0:
+            print("REPOPULATING DATABASE...")
+            # For testing, or first-time use of the web application, reinitialise the database.
+            clear_mappers()
+            metadata.create_all(database_engine)  # Conditionally create database tables.
+            for table in reversed(metadata.sorted_tables):  # Remove any data from the tables.
+                database_engine.execute(table.delete())
+
+            # Generate mappings that map domain model classes to the database tables.
+            map_model_to_tables()
+
+            repository_populate.populate(data_path, repo.repo_instance)
+            print("REPOPULATING DATABASE... FINISHED")
+
+        else:
+            # Solely generate mappings that map domain model classes to the database tables.
+            map_model_to_tables()
 
     # Build the application - these steps require an application context.
     with app.app_context():
@@ -46,5 +88,16 @@ def create_app(test_config=None):
 
         from games.user_info.user_info_bp import user_info_bp
         app.register_blueprint(user_info_bp)
+
+        @app.before_request
+        def before_flask_http_request_function():
+            if isinstance(repo.repo_instance, database_repository.SqlAlchemyRepository):
+                repo.repo_instance.reset_session()
+
+        # Register a tear-down method that will be called after each request has been processed.
+        @app.teardown_appcontext
+        def shutdown_session(exception=None):
+            if isinstance(repo.repo_instance, database_repository.SqlAlchemyRepository):
+                repo.repo_instance.close_session()
 
     return app
