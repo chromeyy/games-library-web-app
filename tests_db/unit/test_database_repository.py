f2aa3486a3f62649ab5649b0a30002efddc71b63
Victoria Ryan <140578626+victoria-ryan-uoa-acc@users.noreply.github.com>
2023-10-14 19:55:53 +1300

    more database repo tests
    not checked yet

--- tests_db/unit/test_database_repository.py
+++ tests_db/unit/test_database_repository.py
@@ -16,14 +16,14 @@ def test_repository_can_add_a_user(session_factory):
 
     assert user3 == user and user3 is user
 
-def test_repository_can_retrieve_a_user(session_factory):
+def test_repository_can_get_a_user(session_factory):
     repo = SqlAlchemyRepository(session_factory)
 
     user = repo.get_user('tori')
     assert user == User('tori', '@Vrya365')
 
 
-def test_repository_can_update_a_user(session_factory):
+def test_repository_can_update_a_user(session_factory): # TO DO
     pass
 
 def test_repository_does_not_retrieve_a_non_existent_user(session_factory):
@@ -51,13 +51,13 @@ def test_repository_can_add_review(session_factory): #DID NOT DO GAME PROPERLY
 def test_repository_can_get_reviews(session_factory): #NOT COMPLETE
     repo = SqlAlchemyRepository(session_factory)
     reviews = repo.get_reviews()
-    assert len(reviews) == xxx #DOTHIS
+    assert len(reviews) == xxx # TO DO
 
 
-def test_repository_game_has_review(session_factory):#DOTHIS
+def test_repository_game_has_review(session_factory):# TO DO
     pass
 
-def test_repository_user_has_review(session_factory):#DOTHIS
+def test_repository_user_has_review(session_factory):# TO DO
     pass
 
 # GAME TESTS
@@ -118,24 +118,48 @@ def test_repository_returns_an_empty_list_for_non_existent_publisher(session_fac
     games_list = repo.get_games_by_publisher('test_fake_publisher12345')
     assert games_list is None
 
-def test_repository_can_get_games_by_search(session_factory):
-    pass
+def test_repository_can_get_games_by_search(session_factory): # TO DO
+    repo = SqlAlchemyRepository(session_factory)
 
-def test_repository_returns_an_empty_list_for_non_existent_search(session_factory):
-    pass
+    game = Game(80085, 'test_game')
+    repo.add_games(game)
 
-def test_repository_can_get_list_of_games(session_factory):
-    pass
+    assert game in repo.get_games_by_search('test_game')
+
+def test_repository_returns_an_empty_list_for_non_existent_search(session_factory): # TO DO
+    repo = SqlAlchemyRepository(session_factory)
+    games_list = repo.get_games_by_search('test_fake_game12345')
+    assert games_list is None
 
 # GENRE TESTS
-def test_repository_can_add_a_genre(session_factory):
-    pass
+def test_repository_can_add_a_genre(session_factory): # CHECK
+    repo = SqlAlchemyRepository(session_factory)
+    game = Game(80085, 'test_game')
+    repo.add_games(game)
+
+    genre2 = Genre('test2')
+    game.add_genre(genre2)
+
+    genres = game.genres
+
+    assert genre2 is Genre and genre2 == game.genres[1]
 
 def test_repository_can_get_a_genre(session_factory):
-    pass
+    repo = SqlAlchemyRepository(session_factory)
+    game = Game(80085, 'test_game')
+    repo.add_games(game)
 
-def test_repository_does_not_get_a_non_existent_genre(session_factory):
-    pass
+    genre1 = Genre('test1')
+    game.add_genre(genre1)
 
-def test_repository_can_get_list_of_genres(session_factory):
-    pass
+    assert game.genres[0] == genre1 and game.genres[0] is Genre
+
+def test_repository_can_get_list_of_genres(session_factory): # CHECK
+    repo = SqlAlchemyRepository(session_factory)
+    game = Game(80085, 'test_game')
+    repo.add_games(game)
+
+    genre1 = Genre('test1')
+    game.add_genre(genre1)
+
+    assert len(game.genres[0]) == 1
