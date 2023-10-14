c77531608673c29dddccb3ed89d67d024f094d3b
Victoria Ryan <140578626+victoria-ryan-uoa-acc@users.noreply.github.com>
2023-10-15 10:46:08 +1300

    initial test_orm.py skeleton

--- tests_db/unit/test_orm.py
+++ tests_db/unit/test_orm.py
@@ -3,3 +3,76 @@ import datetime
 from sqlalchemy.exc import IntegrityError
 
 
+# USER
+def insert_user(empty_session, values=None):
+    pass
+
+
+def insert_users(empty_session, values):
+    pass
+
+
+def make_user():
+    pass
+
+
+def test_loading_of_users(empty_session):
+    pass
+
+
+def test_saving_of_users(empty_session):
+    pass
+
+# GENRE
+def insert_genre(empty_session, values=None):
+    pass
+
+
+def insert_genres(empty_session, values):
+    pass
+
+
+def make_genre():
+    pass
+
+
+# REVIEW
+def insert_review(empty_session, values=None):
+    pass
+
+
+def insert_reviews(empty_session, values):
+    pass
+
+
+def make_review():
+    pass
+
+
+def test_saving_of_review(empty_session):
+    pass
+
+
+def test_loading_review(empty_session):
+    pass
+
+
+# GAME
+
+def insert_game(empty_session, values=None):
+    pass
+
+
+def insert_games(empty_session, values):
+    pass
+
+
+def make_game():
+    pass
+
+def test_saving_of_game(empty_session):
+    pass
+
+
+def test_loading_game(empty_session):
+    pass
