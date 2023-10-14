173d831893de61ea523e79812ba8a8c72c4fd7b1
Victoria Ryan <140578626+victoria-ryan-uoa-acc@users.noreply.github.com>
2023-10-14 18:33:18 +1300

    removing redundant tests

--- tests_db/unit/test_database_repository.py
+++ tests_db/unit/test_database_repository.py
@@ -19,38 +19,29 @@ def test_repository_can_get_list_of_users(session_factory):
 def test_repository_can_add_review(session_factory):
     pass
 
-def test_repository_can_retrieve_review(session_factory):
+def test_repository_can_get_reviews(session_factory):
     pass
 
-def test_repository_does_not_retrieve_a_non_existent_review(session_factory):
+def test_repository_game_has_review(session_factory):
     pass
 
-def test_repository_can_get_list_of_reviews(session_factory):
-    pass
-
-def test_repository_does_not_add_a_review_without_a_user(session_factory):
-    pass
-
-def test_repository_can_retrieve_reviews(session_factory):
-    pass
-
-def test_can_retrieve_an_game_and_add_a_review_to_it(session_factory):
+def test_repository_user_has_review(session_factory):
     pass
 
 # GAME TESTS
 def test_repository_can_add_game(session_factory):
     pass
 
-def test_repository_can_retrieve_game(session_factory):
+def test_repository_can_get_game(session_factory):
     pass
 
-def test_repository_does_not_retrieve_a_non_existent_game(session_factory):
+def test_repository_does_not_get_a_non_existent_game(session_factory):
     pass
 
 def test_repository_can_get_games_by_ids(session_factory):
     pass
 
-def test_repository_does_not_retrieve_games_for_non_existent_id(session_factory):
+def test_repository_does_not_get_games_for_non_existent_id(session_factory):
     pass
 
 def test_repository_can_get_games_by_genre(session_factory):
@@ -79,10 +70,10 @@ def test_repository_can_get_list_of_games(session_factory):
 def test_repository_can_add_a_genre(session_factory):
     pass
 
-def test_repository_can_retrieve_a_genre(session_factory):
+def test_repository_can_get_a_genre(session_factory):
     pass
 
-def test_repository_does_not_retrieve_a_non_existent_genre(session_factory):
+def test_repository_does_not_get_a_non_existent_genre(session_factory):
     pass
 
 def test_repository_can_get_list_of_genres(session_factory):
