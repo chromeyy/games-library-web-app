52db8aaee6e1a73cd195a4e809bc4e3e384b2452
Victoria Ryan <140578626+victoria-ryan-uoa-acc@users.noreply.github.com>
2023-10-15 19:56:55 +1300

    attempt orm but its not working

--- tests_db/unit/test_orm.py
+++ tests_db/unit/test_orm.py
@@ -2,7 +2,7 @@ import pytest
 import datetime
 from sqlalchemy.exc import IntegrityError
 
-from games.domainmodel.model import User, Genre, Review, Game
+from games.domainmodel.model import User, Genre, Review, Game, Publisher
 
 
 # USER
@@ -14,31 +14,19 @@ def insert_user(empty_session, values=None):
         new_name = values[0]
         new_password = values[1]
 
-    empty_session.execute('INSERT INTO users (user_name, password) VALUES (:user_name, :password)',
-                          {'user_name': new_name, 'password': new_password})
-    row = empty_session.execute('SELECT id from users where user_name = :user_name',
-                                {'user_name': new_name}).fetchone()
+    empty_session.execute('INSERT INTO users (username, password) VALUES (:username, :password)',
+                          {'username': new_name, 'password': new_password})
+    row = empty_session.execute('SELECT user_id from users where username = :username',
+                                {'username': new_name}).fetchone()
     return row[0]
-
-def insert_users(empty_session, values):
-    for value in values:
-        empty_session.execute('INSERT INTO users (user_name, password) VALUES (:user_name, :password)',
-                              {'user_name': value[0], 'password': value[1]})
-    rows = list(empty_session.execute('SELECT id from users'))
-    keys = tuple(row[0] for row in rows)
-    return keys
-
-
-def make_user():
-    user = User("test_user", "12345")
-    return user
 
 
 def test_loading_users(empty_session):
     users = list()
     users.append(("test_user_1", "test1234"))
     users.append(("test_user_2", "test54321"))
-    insert_users(empty_session, users)
+    insert_user(empty_session, users[0])
+    insert_user(empty_session, users[1])
 
     expected = [
         User("test_user_1", "test1234"),
@@ -48,94 +36,227 @@ def test_loading_users(empty_session):
 
 
 def test_saving_users(empty_session):
-    user = make_user()
+    user = User("test_user_1", "@Test123")
     empty_session.add(user)
     empty_session.commit()
 
-    rows = list(empty_session.execute('SELECT user_name, password FROM users'))
-    assert rows == [("test_user", "test1234")]
+    rows = list(empty_session.execute('SELECT username, password FROM users'))
+    assert rows == [("test_user_1", "@Test123")]
 
 
 # GENRE
-def insert_genre(empty_session, values=None):
-    pass
+def insert_genre(empty_session, value=None):
+    new_genre = "test_genre"
+
+    if value is not None:
+        new_genre = value
+
+    empty_session.execute('INSERT INTO genres (genre_name) VALUES (:genre_name)',
+                          {'genre_name': new_genre})
+    row = empty_session.execute('SELECT genre_name from genres where genre_name = :genre_name',
+                                {'genre_name': new_genre}).fetchone()
+    return row[0]
 
 
-def insert_genres(empty_session, values):
-    pass
-
-
-def make_genre():
+def test_saving_genre(empty_session):
     genre = Genre("test_genre")
-    return genre
+    empty_session.add(genre)
+    empty_session.commit()
+
+    rows = list(empty_session.execute('SELECT genre_name FROM genres'))
+    assert rows == [("test_genre",)]
+
+
+def test_loading_genre(empty_session):  # keeps failing
+    genres = list()
+    genres.append("test_genre")
+    genres.append("test_genre_2")
+
+    insert_genre(empty_session, genres[0])
+    insert_genre(empty_session, genres[1])
+    expected = [
+        Genre("test_genre"),
+        Genre("test_genre_2")
+    ]
+    assert empty_session.query(Genre).all() == expected
 
 
 # REVIEW
 def insert_review(empty_session, values=None):
-    pass
+    new_user = 80085
+    new_game_id = 80085
+    new_rating = 5
+    new_comment = "great!"
 
+    if values is not None:
+        new_user = values[0]
+        new_game_id = values[1].game_id
+        new_rating = values[2]
+        new_comment = values[3]
 
-def insert_reviews(empty_session, values):
-    pass
+    user_id = empty_session.execute('SELECT user_id FROM users WHERE username = :username',
+                                    {'username': new_user.username}).fetchone()
 
+    empty_session.execute('INSERT INTO reviews (game_id, user_id, review_text, rating) VALUES '
+                          '(:game_id, :user_id, :review_text, :rating)',
+                          {'game_id': new_game_id, 'user_id': user_id, 'review_text': new_comment,
+                           'rating': new_rating})
 
-def make_review():
-    test_user = User('test', 'test_password')
-    test_game = Game(100010001, 'test')
-    review = Review(test_user, test_game, 1, "test_comment")
-    return review
+    row = empty_session.execute('SELECT rating FROM reviews Where review_text = :review_text',
+                                {'review_text': new_comment}).fetchone()
+    return row[0]
 
 
 def test_saving_review(empty_session):
-    pass
+    user = User('test1', 'Test1234@')
+    game = Game(80085, 'test_game')
+    game.release_date = "Oct 18, 2003"
+    game.price = float(5)
+    game.description = "test desc"
+    game.image_url = "url"
+    publisher = Publisher("testPub")
+    game.publisher = publisher
+    genre = Genre("testGenre")
+    game.add_genre(genre)
+    test_review = Review(user, game, 5, "test_review")
+    empty_session.add(test_review)
+    empty_session.commit()
+
+    rows = list(empty_session.execute('SELECT game_id, review_text FROM reviews'))
+    assert rows == [(game.game_id, "test_review",)]
 
 
 def test_loading_review(empty_session):
-    pass
+    reviews = list()
+    game = Game(80086, 'test_game')
+    game.release_date = "Oct 18, 2003"
+    game.price = float(5)
+    game.description = "test desc"
+    game.image_url = "url"
+    publisher = Publisher("testPub")
+    game.publisher = publisher
+    game.add_genre(Genre('test_genre'))
+
+    user = User("PotatoMan", "Potato#1234")
+
+    user2 = User("TotatoMan", "Totato#1234")
+
+    insert_review(empty_session, (user, game, 5, "great!"))  # adding a new review
+    insert_review(empty_session, (user2, game, 3, "not so amazing"))  # adding a second new review
+    expected = [
+        Review(user, game, 5, "great!"),
+        Review(user2, game, 3, "not so amazing")
+    ]
+    assert empty_session.query(Review).all() == expected
 
 
 # FAVOURITES
-def insert_favourite(empty_session, values=None):
-    pass
-
-
-def insert_favourites(empty_session, values):
-    pass
-
-
 def test_saving_favourite(empty_session):
-    pass
+    user = User('test1', 'Test1234@')
+    game = Game(80085, 'test_game')
+    game.release_date = "Oct 18, 2003"
+    game.price = float(5)
+    game.description = "test desc"
+    game.image_url = "url"
+    publisher = Publisher("testPub")
+    game.publisher = publisher
+    genre = Genre("testGenre")
+    game.add_genre(genre)
+    user.add_favourite_game(game)
+    empty_session.add(user)
+    empty_session.commit()
+
+    rows = list(empty_session.execute('SELECT game_id FROM favourites'))
+    assert rows == [(game.game_id,)]
 
 
-def test_loading_favourite(empty_session):
-    pass
+# def insert_favourite(empty_session, values=None):
+
+
+# def test_loading_favourite(empty_session):
 
 
 # GAME
 
 def insert_game(empty_session, values=None):
-    pass
+    game_id = 80086
+    game_title = "test_game"
+    release_date = "Oct 18, 2003"
+    price = float(5)
+    description = "test desc"
+    image_url = "url"
+    publisher = Publisher("testPub")
+    genre = Genre('test_genre')
 
+    if value is not None:
+        game_id = values[0]
+        game_title = values[1]
+        release_date = values[2]
+        price = values[3]
+        description = values[4]
+        image_url = values[5]
+        publisher = values[6]
 
-def insert_games(empty_session, values):
-    pass
-
-
-def make_game():
-    game = Game(100196, 'test_game')
-    return game
+    empty_session.execute('INSERT INTO games (game_id, game_title, game_price, release_date, game_description,'
+                          'game_image_url, publisher_name) VALUES (:game_id, :game_title, :game_price, :release_date, '
+                          ':game_description, :game_image_url, :publisher_name)',
+                          {'game_id': game_id, 'game_title': game_title, 'game_price': price, 'release_date': release_date,
+                           'game_description': description, 'game_image_url': image_url, 'publisher_name': publisher})
+    row = empty_session.execute('SELECT game_id from games where game_title = :game_title',
+                                {'game_title': game_title}).fetchone()
+    return row[0]
 
 
 def test_saving_game(empty_session):
-    pass
+    new_title = 'test_game'
+    new_id = 80085
+    game = Game(new_id, new_title)
+    game.release_date = "Oct 18, 2003"
+    game.price = float(5)
+    game.description = "test desc"
+    game.image_url = "url"
+    publisher = Publisher("testPub")
+    game.publisher = publisher
+
+    empty_session.add(game)
+    empty_session.commit()
+
+    rows = list(empty_session.execute('SELECT game_id, game_title FROM games'))
+    assert rows == [(new_id, new_title,)]
 
 
 def test_loading_game(empty_session):
-    pass
+    game_id = 80086
+    game_title = "test_game"
+    release_date = "Oct 18, 2003"
+    price = float(5)
+    description = "test desc"
+    image_url = "url"
+    publisher = Publisher("testPub")
+    genre = Genre('test_genre')
+
+    insert_game(empty_session, (empty_session, game_id, game_title, release_date, price, description, image_url, publisher, genre))
+    expected = ["test_game"]
+    assert empty_session.query(Genre).all() == expected
 
 
 def test_saving_game_with_genre(empty_session):
-    pass
+    new_title = 'test_game'
+    new_id = 80085
+    game = Game(new_id, new_title)
+    game.release_date = "Oct 18, 2003"
+    game.price = float(5)
+    game.description = "test desc"
+    game.image_url = "url"
+    publisher = Publisher("testPub")
+    game.publisher = publisher
+    genre = Genre("testGenre")
+    game.add_genre(genre)
 
-def test_loading_game_with_genre(empty_session):
-    pass
+    empty_session.add(game)
+    empty_session.commit()
+
+    rows = list(empty_session.execute('SELECT game_id, genre_name FROM game_genres'))
+    assert rows == [(new_id, "testGenre",)]
+
+# def test_loading_game_with_genre(empty_session):
