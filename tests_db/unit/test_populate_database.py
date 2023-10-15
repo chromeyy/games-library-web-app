990a2c577e3ef7744fa46f34eac333b2861e189f
Victoria Ryan <140578626+victoria-ryan-uoa-acc@users.noreply.github.com>
2023-10-15 16:51:38 +1300

    test_populate_database tests + passing

--- tests_db/unit/test_populate_database.py
+++ tests_db/unit/test_populate_database.py
@@ -1,83 +1,81 @@
 from sqlalchemy import select, inspect
 
 from games import metadata
+from games.domainmodel.model import Review, Game, User, Genre
 
-#add comments
 
 def test_database_populate_inspect_table_names(database_engine):
     inspector = inspect(database_engine)
-    assert inspector.get_table_names() == ['game_genres', 'users', 'games', 'reviews', 'genres']
+    assert (inspector.get_table_names() == ['favourites', 'game_genres', 'games',
+                                            'genres', 'publishers', 'reviews','users'])
 
 
 def test_database_populate_select_all_users(database_engine):
     inspector = inspect(database_engine)
-    name_of_users_table = inspector.get_table_names()[1]
+    name_of_users_table = inspector.get_table_names()[6]
 
     with database_engine.connect() as connection:
-        # query for records in table users
         select_statement = select([metadata.tables[name_of_users_table]])
         result = connection.execute(select_statement)
 
         all_users = []
         for row in result:
-            all_users.append(row['user_name'])
+            all_users.append(row['username'])
 
-        assert all_users == ['kevin', 'tom']
+        assert all_users == ['kevin', 'tom', 'tori']
 
 
 def test_database_populate_select_all_genres(database_engine):
-    # inspector = inspect(database_engine)
-    # genres_table = inspector.get_table_names()[5]
+    inspector = inspect(database_engine)
+    genres_table = inspector.get_table_names()[3]
 
-    # with database_engine.connect() as connection:
-        # select_statement = select([metadata.tables[genres_table]])
-        # result = connection.execute(select_statement)
+    with database_engine.connect() as connection:
+        select_statement = select([metadata.tables[genres_table]])
+        result = connection.execute(select_statement)
 
-        # all_genres = []
-        # for row in result:
-        #    all_reviews.append((row['genre']))
 
-        # nr_genres = len(all_genres)
-        # assert nr_genres == 6 <--- fix this
+        all_genres = []
+        for row in result:
+            all_genres.append((row['genre_name']))
 
-        # assert all_reviews0] == <--- fix this
-        pass
+        nr_genres = len(all_genres)
+        assert nr_genres == 24
+        assert all_genres[0] == 'Action'
+
 
 
 def test_database_populate_select_all_reviews(database_engine):
-    # inspector = inspect(database_engine)
-    # reviews_table = inspector.get_table_names()[3]
+    inspector = inspect(database_engine)
+    reviews_table = inspector.get_table_names()[5]
 
-    # with database_engine.connect() as connection:
-        # select_statement = select([metadata.tables[reviews_table]])
-        # result = connection.execute(select_statement)
+    with database_engine.connect() as connection:
+        select_statement = select([metadata.tables[reviews_table]])
+        result = connection.execute(select_statement)
 
-        # all_reviews = []
-        # for row in result:
-        #    all_reviews.append((row['user'], row['game'], row['rating'], row['comment']))
+        all_reviews = []
+        for row in result:
+            all_reviews.append((row['review_text']))
 
-        # nr_reviews = len(all_reviews)
-        # assert nr_reviews == 6 <--- fix this
+        nr_reviews = len(all_reviews)
+        assert nr_reviews == 3
 
-        # assert all_reviews0] == <--- fix this
-        pass
+        assert all_reviews[0] == 'Super cool'
 
 
 def test_database_populate_select_all_games(database_engine):
-    # inspector = inspect(database_engine)
-    # name_of_games_table = inspector.get_table_names()[2]
+    inspector = inspect(database_engine)
+    name_of_games_table = inspector.get_table_names()[2]
 
-    # with database_engine.connect() as connection:
-        # select_statement = select([metadata.tables[name_of_games_table]])
-        # result = connection.execute(select_statement)
+    with database_engine.connect() as connection:
+        select_statement = select([metadata.tables[name_of_games_table]])
+        result = connection.execute(select_statement)
 
-        # all_games = []
-        # for row in result:
-        #    all_games.append((row['id'], row['title']))
+        all_games = []
+        for row in result:
+           all_games.append(row['game_title'])
 
-        # nr_games = len(all_games)
-        # assert nr_games == 6 <--- fix this
+        nr_games = len(all_games)
+        assert nr_games == 877
 
-        # assert all_games[0] == <--- fix this
-        pass
+        assert all_games[0] == 'Xpand Rally'
 
